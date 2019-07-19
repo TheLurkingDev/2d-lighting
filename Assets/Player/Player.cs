330ba3d0e284f1e048e80a6e7ffe2853628c2ce5
Darren Cook <thelurkingdev@gmail.com>
2019-07-18 23:59:02 -0400

    Added footstep audio to player.

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Transform _transform;
+    private AudioSource _audioSource;
+
+    [SerializeField]
+    private AudioClip _footstepSound;
+
+    private float _footStepSoundAudioScale = 1f;
     
     // Start is called before the first frame update
     void Start()
     {
         _transform = this.gameObject.transform;
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,18 +25,22 @@ public class Player : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.UpArrow))
             {
                 _transform.Translate(Vector3.up);
+                _audioSource.PlayOneShot(_footstepSound, _footStepSoundAudioScale);
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 _transform.Translate(Vector3.down);
+                _audioSource.PlayOneShot(_footstepSound, _footStepSoundAudioScale);
             }
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 _transform.Translate(Vector3.left);
+                _audioSource.PlayOneShot(_footstepSound, _footStepSoundAudioScale);
             }
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 _transform.Translate(Vector3.right);
+                _audioSource.PlayOneShot(_footstepSound, _footStepSoundAudioScale);
             }
         }
     }
